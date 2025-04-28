67a9b6e10ca287052c4cd469f233f1e5172bae13
champagne101 <99389926+champagne101@users.noreply.github.com>
2025-04-28 21:07:28 +0200

    Update fullSchoolSystem.sol

--- fullSchoolSystem.sol
+++ fullSchoolSystem.sol
@@ -1,1 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract School {
+    string public schoolName;
+
+    constructor(string memory _name) {
+        schoolName = _name;
+    }
+
+    function getSchoolName() public view returns (string memory) {
+        return schoolName;
+    }
+}
+
+contract StudentsSystem is School {
+
+    struct Student {
+        string name;
+        uint mathGrade;
+        uint scienceGrade;
+    }
+
+    mapping(uint => Student) public students;
+
+    constructor(string memory _schoolName) School(_schoolName) {
+        
+    }
+
+    function addStudent(uint _id, string calldata _name, uint _mathGrade, uint _scienceGrade) public {
+        students[_id] = Student(_name, _mathGrade, _scienceGrade);
+    }
+
+    function getStudent(uint _id) public view returns (string memory, uint, uint) {
+        Student memory s = students[_id];
+        return (s.name, s.mathGrade, s.scienceGrade);
+    }
+}
+
+
+contract AdminControl {
+    address public admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not admin");
+        _;
+    }
+
+    function changeAdmin(address _newAdmin) public onlyAdmin {
+        admin = _newAdmin;
+    }
+}
+
+
+contract FullSchoolSystem is StudentsSystem, AdminControl {
+
+    constructor(string memory _schoolName) StudentsSystem(_schoolName) AdminControl() {
+        
+    }
+
+    function getFullDetails(uint _id) public view returns (string memory, uint, uint, string memory, address) {
+        (string memory name, uint mathGrade, uint scienceGrade) = getStudent(_id);
+        return (name, mathGrade, scienceGrade, getSchoolName(), admin);
+    }
+}
 
